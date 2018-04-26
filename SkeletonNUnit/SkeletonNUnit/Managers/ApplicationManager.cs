52e99b831a62171e3175e0027f8dd2b61925cc29
MINSK\Aliaksandr_Stratsiai <Aliaksandr_Stratsiaichuk@epam.com>
2018-04-26 15:56:58 +0300

    Update

---
+++ SkeletonNUnit/SkeletonNUnit/Managers/ApplicationManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Repository.Hierarchy;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using TestCore.Managers.Interfaces;
+
+namespace SkeletonNUnit.Managers
+{
+    public class ApplicationManager: IApplicationManager
+    {
+
+        private string baseUrl;
+
+        /// <summary>
+        /// The capabilities.
+        /// </summary>
+        private ICapabilities capabilities;
+
+        /// <summary>
+        /// The hub url.
+        /// </summary>
+        private string hubUrl;
+
+        /// <summary>
+        /// The _pages.
+        /// </summary>
+//        private PageManager pages;
+
+        /// <summary>
+        /// The main.
+        /// </summary>
+//        private MainHelper main;
+
+        /// <summary>
+        /// Gets or sets the Log4Net logger.
+        /// </summary>
+        public Logger Logger { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationManager"/> class.
+        /// </summary>
+        /// <param name="capabilities">
+        /// The capabilities.
+        /// </param>
+        /// <param name="baseUrl">
+        /// The base url.
+        /// </param>
+        /// <param name="hubUrl">
+        /// The hub url.
+        /// </param>
+        public ApplicationManager(ICapabilities capabilities, string baseUrl, string hubUrl)
+        {
+            this.capabilities = capabilities;
+            this.baseUrl = baseUrl;
+            this.hubUrl = hubUrl;
+        }
+    }
+}
--- SkeletonNUnit/SkeletonNUnit/UnitTest1.cs
+++ SkeletonNUnit/SkeletonNUnit/UnitTest1.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using ReportPortal.Shared;
 
 namespace SkeletonNUnit
 {
     [TestFixture]
     public class UnitTest1
     {
+        protected log4net.ILog Log4NetLogger = log4net.LogManager.GetLogger(typeof(UnitTest1));
+
         [Test]
         public void TestMethod1()
         {
+
+            Bridge.LogMessage(ReportPortal.Client.Models.LogLevel.Trace, "class1 test1 log message");
+            Log4NetLogger.Info("My log message from Log4Net");
+//            var filePath = TestContext.CurrentContext.TestDirectory + "\\dog.png";
+//            Bridge.LogMessage(ReportPortal.Client.Models.LogLevel.Info, "my dog RP native {rp#file#" + filePath + "}");
+//            Log4NetLogger.Info("my dog Log4net {rp#file#" + filePath + "}");
+            Assert.That(true);
         }
     }
 }
