1b59ccf9ab8d2c30b7867f29fa480bc1325be9d1
MINSK\Aliaksandr_Stratsiai <Aliaksandr_Stratsiaichuk@epam.com>
2018-04-26 14:44:08 +0300

    Added TestCore project

---
+++ TestCore/Service/DriverExtensions.cs
@@ -0,0 +1,138 @@
+﻿
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestCore.Service
+{
+    /// <summary>
+    /// The webdriver extensions.
+    /// </summary>
+    public static class DriverExtensions
+    {
+        /// <summary>
+        /// The wait for page ready.
+        /// </summary>
+        /// <param name="driver">
+        /// The driver.
+        /// </param>
+        /// <param name="timeoutSeconds">
+        /// The timeout Seconds.
+        /// </param>
+        public static void WaitForPageReady(this IWebDriver driver, int timeoutSeconds = 30)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutSeconds));
+
+            // Check if document is ready
+            try
+            {
+                wait.Until(CustomExpectedConditions.PageReadyCondition);
+            }
+            catch (WebDriverTimeoutException e)
+            {
+                throw new Exception(e.Message + " .Page can't be completely loaded");
+            }
+        }
+
+        /// <summary>
+        /// The double click.
+        /// </summary>
+        /// <param name="driver">
+        /// The driver.
+        /// </param>
+        /// <param name="locator">
+        /// The locator.
+        /// </param>
+        public static void DoubleClick(this IWebDriver driver, By locator)
+        {
+            Actions action = new Actions(driver);
+            action.DoubleClick(driver.FindElement(locator)).Perform();
+        }
+
+        /// <summary>
+        /// The double click.
+        /// </summary>
+        /// <param name="driver">
+        /// The driver.
+        /// </param>
+        /// <param name="element">
+        /// The element.
+        /// </param>
+        public static void DoubleClick(this IWebDriver driver, IWebElement element)
+        {
+            Actions action = new Actions(driver);
+            action.DoubleClick(element).Perform();
+        }
+
+        /// <summary>
+        /// The hover element.
+        /// </summary>
+        /// <param name="driver">
+        /// The driver.
+        /// </param>
+        /// <param name="locator">
+        /// The locator.
+        /// </param>
+        public static void HoverElement(this IWebDriver driver, By locator)
+        {
+            Actions action = new Actions(driver);
+            action.MoveToElement(driver.FindElement(locator)).Perform();
+        }
+
+        /// <summary>
+        /// The hover element.
+        /// </summary>
+        /// <param name="driver">
+        /// The driver.
+        /// </param>
+        /// <param name="element">
+        /// The element.
+        /// </param>
+        public static void HoverElement(this IWebDriver driver, IWebElement element)
+        {
+            Actions action = new Actions(driver);
+            action.MoveToElement(element).Perform();
+        }
+
+        /// <summary>
+        /// The hover element and click.
+        /// </summary>
+        /// <param name="driver">
+        /// The driver.
+        /// </param>
+        /// <param name="element">
+        /// The element.
+        /// </param>
+        public static void HoverElementAndClick(this IWebDriver driver, IWebElement element)
+        {
+            Actions action = new Actions(driver);
+            action.MoveToElement(element).Click().Perform();
+        }
+
+        /// <summary>
+        /// Disable implicit timeout.
+        /// </summary>
+        /// <param name="driver">
+        /// The driver.
+        /// </param>
+        public static void DisableTimeout(this IWebDriver driver)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+        }
+
+        /// <summary>
+        /// Set implicit timeout.
+        /// </summary>
+        /// <param name="driver">
+        /// The driver.
+        /// </param>
+        /// <param name="timeoutSec">
+        /// The timeout sec.
+        /// </param>
+        public static void SetTimeout(this IWebDriver driver, int timeoutSec = 10)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(timeoutSec);
+        }
+    }
+}
---
+++ TestCore/Service/ServiceMethods.cs
@@ -0,0 +1,79 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCore.Service
+{
+    /// <summary>
+    /// The service methods.
+    /// </summary>
+    public class ServiceMethods
+    {
+        /// <summary>
+        /// Get random numbers.
+        /// </summary>
+        /// <param name="from">
+        /// The from.
+        /// </param>
+        /// <param name="toCount">
+        /// The to count.
+        /// </param>
+        /// <param name="numberOfElements">
+        /// The number of elements.
+        /// </param>
+        /// <returns>
+        /// The <see cref="List"/>.
+        /// </returns>
+        public static List<int> GetRandomNumbers(int from, int toCount, int numberOfElements)
+        {
+            Random random = new Random();
+            return Enumerable.Range(from, toCount).OrderBy(n => random.Next()).Take(numberOfElements).ToList();
+        }
+
+        /// <summary>
+        /// The list to string converter.
+        /// </summary>
+        /// <param name="list">
+        /// The list.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <returns>
+        /// The string result <see cref="string"/>.
+        /// </returns>
+        public static string ListToString<T>(List<T> list)
+        {
+            StringBuilder itemsString = new StringBuilder();
+            foreach (var item in list)
+            {
+                itemsString.Append(Environment.NewLine + item);
+            }
+
+            return itemsString.ToString();
+        }
+
+        /// <summary>
+        /// The compare lists.
+        /// </summary>
+        /// <param name="list1">
+        /// The list 1.
+        /// </param>
+        /// <param name="list2">
+        /// The list 2.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <returns>
+        /// If lists are equal disregarding seqience<see cref="bool"/>.
+        /// </returns>
+        public static bool CompareLists<T>(List<T> list1, List<T> list2)
+        {
+            var hashSet1 = new HashSet<T>(list1);
+            var hashSet2 = new HashSet<T>(list2);
+            return hashSet1.SetEquals(hashSet2) && list1.Count.Equals(list2.Count);
+        }
+    }
+}
