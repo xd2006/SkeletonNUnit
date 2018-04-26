210196ce5ab3a666103a3d590b1376039af55e6a
MINSK\Aliaksandr_Stratsiai <Aliaksandr_Stratsiaichuk@epam.com>
2018-04-26 17:22:47 +0300

    Report portal integration

--- SkeletonNUnit/SkeletonNUnit/Tests/UnitTest1.cs
+++ SkeletonNUnit/SkeletonNUnit/Tests/UnitTest1.cs
@@ -15,9 +15,9 @@ namespace SkeletonNUnit
 
             Bridge.LogMessage(ReportPortal.Client.Models.LogLevel.Trace, "class1 test1 log message");
             Log4NetLogger.Info("My log message from Log4Net");
-//            var filePath = TestContext.CurrentContext.TestDirectory + "\\dog.png";
-//            Bridge.LogMessage(ReportPortal.Client.Models.LogLevel.Info, "my dog RP native {rp#file#" + filePath + "}");
-//            Log4NetLogger.Info("my dog Log4net {rp#file#" + filePath + "}");
+            var filePath = TestContext.CurrentContext.TestDirectory + "\\dog.png";
+            Bridge.LogMessage(ReportPortal.Client.Models.LogLevel.Info, "my dog RP native {rp#file#" + filePath + "}");
+            Log4NetLogger.Info("my dog Log4net {rp#file#" + filePath + "}");
             Assert.That(true);
         }
     }
